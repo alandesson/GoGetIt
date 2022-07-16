59b4472306bf2d858e75a34fb03ea773bff7a3d6
Felipe Buson <fxbuson@gmail.com>
2022-07-16 13:36:45 -0300

    Obstaculos moveis
    Adicionado o código para os obstáculos móveis (sweeper, rotator, bouncer, const_force) baseados em KinematicBodies e Areas que ainda precisam ser decorados e implementados em level design.

--- Rotator.gd
+++ Rotator.gd
@@ -5,14 +5,28 @@ extends KinematicBody
 # var a = 2
 # var b = "text"
 
-export var rot_speed = 5
+export var rot_speed = 20
+var actual_speed = rot_speed
+export var pendulate = false
+
+onready var initial_rotation = rotation
+var rot_dir = 1
+export(int, 10, 180) var max_angle = 90
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-func _physics_process(delta):
-	rotate_y((0.01 * rot_speed)/(2*PI))
+func _physics_process(delta):	
+	
+	rotate(to_global(Vector3.UP) - translation, (0.01 * actual_speed * rot_dir)/(2*PI))
+	var angle = rotation.distance_to(initial_rotation)
+	if angle >= deg2rad(max_angle) :
+		rot_dir = rot_dir * -1
+	
+	if pendulate:
+		actual_speed = rot_speed * (1 - angle/deg2rad(max_angle+5))
+	
 	move_and_slide(Vector3.ZERO)
 
 
