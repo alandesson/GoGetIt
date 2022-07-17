9f1a2af5149ad67f7551f6b8b594889a12008c96
Felipe Buson <fxbuson@gmail.com>
2022-07-17 10:51:49 -0300

    Level 2
    Adicionado level 2 e algumas alterações na camera e no player para poder resetar níveis e centralizar a visão no player

--- Scripts/Rotator.gd
+++ Scripts/Rotator.gd
@@ -6,19 +6,19 @@ extends KinematicBody
 # var b = "text"
 
 export var rot_speed = 20
-var actual_speed = rot_speed
+onready var actual_speed = rot_speed
 export var pendulate = false
 
 onready var initial_rotation = to_global(Vector3.RIGHT) - translation
-var rot_dir = 1
-export(int, 10, 180) var max_angle = 90
+export(int, -1, 1) var rot_dir = 1
+export(int, 10, 190) var max_angle = 90
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func _physics_process(delta):
-	rotate(to_global(Vector3.UP) - translation, (0.01 * actual_speed * rot_dir)/(2*PI))
+	rotate(to_global(Vector3.UP) - translation, deg2rad(0.01 * actual_speed * rot_dir))
 	var angle = initial_rotation.angle_to(to_global(Vector3.RIGHT) - translation)
 	if angle >= deg2rad(max_angle) :
 		rot_dir = rot_dir * -1
